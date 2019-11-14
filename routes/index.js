f1d59939be25ba95789124896e0b1196f797f0b0
Joanna Huang <joannathhuang@gmail.com>
2019-11-14 17:59:40 -0500

    almost there

--- routes/index.js
+++ routes/index.js
@@ -5,35 +5,57 @@ const {Page}=require("../models");
 const {User}=require("../models");
 const index = require("../views/index");
 
-// db.authenticate().
-// then(() => {
-//   console.log('connected to the database');
-// })
-// const init = async() => {
-//     await models.db.sync({force: true})
+router.get('/users/:id', async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        const userData = await User.findByPk(id)
+        const pageData = await Page.findByPk(userData.id)
+        res.send(index.userPages(userData, pageData))
+        // console.log(userdata.id)
+        // console.log(pageData)
+    } catch (error) {
+        next(error)
+    }
+    console.log(id)
+    
+    
+})
 
-// }
-// //models.db.sync({force: true})
-// init();
 router.get('/add', (req, res, next) => {
     res.send(index.addPage())
+})
+
+router.get('/users', async (req, res, next) => {
+    try {
+        const users = await User.findAll();
+        console.log(users)
+        res.send(index.userList(users))
+    } catch (error) {
+        next(error)
+    }
+    
 })
 
 router.get('/:slug', async(req, res, next) => {
     let slug=req.params.slug;
     try{
-    const foundSlug=await Page.findOne({
-        where: { slug : slug}
+        const foundId = await Page.findOne({
+            attributes: ['authorId'], 
+            where: {slug : slug}
+        
+        })
+        console.log(foundId.authorId)
+        const foundName = await User.findByPk(foundId.authorId)
+        console.log(foundName)
+        const foundSlug=await Page.findOne({
+            where: { slug : slug}
     });
-    res.send(index.wikiPage(foundSlug))
-}catch(err){next(err)}
+        res.send(index.wikiPage(foundSlug, foundName))
+    }catch(err){next(err)}
 
-  });
+    });
 
-router.get('/users', (req, res, next) => {
 
-    res.send(index.userList())
-})
 
 router.get('/', async(req, res, next) => {
     // console.log(user)
@@ -44,6 +66,9 @@ router.get('/', async(req, res, next) => {
     }catch(err){next(err)}
 
 })
+
+
+
 function getSlug(title){
     let r=/[^A-Za-z]/g;
     return title.replace(r,'_');
